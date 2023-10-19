cf65002052bc846657ca21ec49446481d3ce9cfe
Vlad Belyaev <mr.vladiss@mail.ru>
2023-10-19 17:18:18 +0300

    Добавлен метод вывода натуральных чисел в промежутке

--- HomeW9/Program.cs
+++ HomeW9/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывод натуральных чисел в промежутке от N до 1
+
+void ShowDigital(int n)
+{
+    if (n >= 1)
+    {
+        Console.Write(n + "," + " ");
+        ShowDigital(n-1);
+    }
+}
+Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"N = {n} -> ");
+ShowDigital(n);
+
