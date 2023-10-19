e7c22e1dda98f2aa6e5313f8a0b1e385a923c805
Vlad Belyaev <mr.vladiss@mail.ru>
2023-10-19 17:20:42 +0300

    Добавлен метод нахождения суммы натуральных элементов в промежутке

--- HomeW9/Program.cs
+++ HomeW9/Program.cs
@@ -13,3 +13,25 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write($"N = {n} -> ");
 ShowDigital(n);
 
+// Сумма натуральных элементов в промежутке от M до N
+
+int SumNumbers(int m, int n)
+{
+    if (n > m)
+    {
+        if (m == n) return m;
+    else return n + SumNumbers(m,n-1);
+    }
+    else
+    {
+       if (m == n) return n;
+    else return m + SumNumbers(m-1,n);
+    }
+}
+Console.WriteLine("Input M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M = {m}; N = {n} -> {SumNumbers(m,n)}");
+
