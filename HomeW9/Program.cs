4d4357260c50060813a09a02c3d6e63a7d50059f
Vlad Belyaev <mr.vladiss@mail.ru>
2023-10-19 17:22:20 +0300

    Добавлен метод вычисления функции Аккермана

--- HomeW9/Program.cs
+++ HomeW9/Program.cs
@@ -35,3 +35,25 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"M = {m}; N = {n} -> {SumNumbers(m,n)}");
 
+// Вычисление функции Аккермана с помощью рекурсии
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+Console.WriteLine("Input m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"m = {m}; n = {n} -> A(m,n) = {Ackermann(m, n)}");
+
